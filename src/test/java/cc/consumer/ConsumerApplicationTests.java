0f91be25adefde1792feff7cc79c6187da22672d
CC <aliezarean@gmail.com>
2021-01-25 09:05:28 +0330

    start

---
+++ src/test/java/cc/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
