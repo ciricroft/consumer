1c5b7cb54f007a1015966809bee5574493f65f1c
CC <aliezarean@gmail.com>
2021-01-25 10:02:03 +0330

    finished

--- src/main/java/controls/ListeningController.java
+++ src/main/java/controls/ListeningController.java
@@ -14,6 +14,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+
+
+//as long as we cant consume kafka objects on recieve and we have to process them each minuet according to their send time we are gonna save them in a queue for later consumption
+
+
+
+
 @EnableScheduling
 @Controller
 @ComponentScan
@@ -35,13 +42,31 @@ public class ListeningController {
 
     @Scheduled(fixedRate = 60000)
     void consume () {
-        if(objQueue.isEmpty())
+        //checking if we got any new objects from kafka
+        if(objQueue.isEmpty()) {
+            System.out.println("no given inputs in that minuet");
             return;
+        }
 
-        Obj currentObj=objQueue.remove();
+
+        long totalVal=0;
+        Obj currentObj;
+        //getting the first object in queue
+        {
+            currentObj=objQueue.remove();
+        }
+        //checking if lastchecked date was initialized
         if(lastCheckedDate.getTime()==0) {
             lastCheckedDate.setTime(currentObj.getDate().getTime());
         }
+        //changing the last checked time to last time+60 seconds so we can use that as a boundry for consuming objects
+        lastCheckedDate.setTime(lastCheckedDate.getTime()+60000);
+
+        //calculating total val
+        while(objQueue.peek().getDate().compareTo(lastCheckedDate)<0) {
+            totalVal+=objQueue.remove().getValue();
+        }
+        System.out.println("the total value of given objects in the processed minute is: "+totalVal);
     }
 
 }
